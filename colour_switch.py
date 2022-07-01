c7f3fce08b961a293d9b20c61fc68c4aa72591d2
David Melamed <david.melamed@web.de>
2022-07-01 13:34:35 +0200

    added switch in seperate file

---
+++ colour_switch.py
@@ -0,0 +1,28 @@
+from colorama import Fore
+
+def A():
+    return Fore.RED + "A"
+
+def C():
+    return Fore.YELLOW + "C"
+
+def G():
+    return Fore.GREEN + "G" 
+
+def T():
+    return Fore.BLUE + "T"
+
+
+switcher = {
+        "A": A,
+        "C": C,
+        "G": G,
+        "T": T
+    }
+
+
+def letters_to_colours(argument):
+    # Get the function from switcher dictionary
+    func = switcher.get(argument, "nothing")
+    # Execute the function
+    return func()
--- core.py
+++ core.py
@@ -1,9 +1,16 @@
 from multiprocessing.dummy import Array
 from tokenize import String
-
+import colour_switch
 
 class DNA():
     def __init__(self, sequence): # add helix directions with fifth & third
-        self.sequence = String(sequence)
+        self.sequence = str(sequence).upper()
         
-class 
+    def print(self):
+        colouredString = []
+        # iterate through sequenz
+        for letter in self.sequence:
+            # for each character chech which colour to add
+            colouredString.append(colour_switch.letters_to_colours(letter))
+        print(colouredString)
+            
