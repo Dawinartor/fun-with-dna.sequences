24f1989543dacb2de137bc7416dabac6e4e931c5
David Melamed <david.melamed@web.de>
2022-07-01 13:49:30 +0200

    added coloured print of dna sequence

--- colour_switch.py
+++ colour_switch.py
@@ -1,4 +1,5 @@
-from colorama import Fore
+from colorama import Fore, init
+init()
 
 def A():
     return Fore.RED + "A"
--- core.py
+++ core.py
@@ -1,16 +1,13 @@
-from multiprocessing.dummy import Array
 from tokenize import String
 import colour_switch
+
 
 class DNA():
     def __init__(self, sequence): # add helix directions with fifth & third
         self.sequence = str(sequence).upper()
         
     def print(self):
-        colouredString = []
         # iterate through sequenz
         for letter in self.sequence:
-            # for each character chech which colour to add
-            colouredString.append(colour_switch.letters_to_colours(letter))
-        print(colouredString)
-            
+            # for each character check which colour to add
+            print(colour_switch.letters_to_colours(letter), end='')
